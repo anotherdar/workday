96f793cd3ffd76f8109668e3832dd5a4a28d6430
AnotherDar <rosariodaren9@gmail.com>
2024-08-31 15:25:56 -0400

    feat(root): add icons

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {View} from 'react-native';
 import { Text } from '../../components/Text';
 
+
 export const HomeScreen: React.FC = () => {
     return <View>
         <Text>Panaderia</Text>
